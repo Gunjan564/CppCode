c7579a8389bb1d2eecf0e7c6565ee6a63286bd3a
Gunjan <>
2024-07-04 14:47:07 +0530

    hello

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,29 +1,14 @@
-// int arr[3];
-//         for(int i=0;i<3;i++){
-//             cin>>arr[i];
-//         }
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+int F(int n){
+  if(n==1||n==2){
+    return 1;
+  }
+  else
+  return F(n-1)+F(n-2);
+}
 int main()
 {
-    int t;
-    cin>>t;
-    int a,b,c;
-    while(t--){
-        cin>>a>>b>>c;
-    
-    int n; 
-    if((a<b&&b<c)||(c<b&&b<a)){
-        n=b;
-    }
-    else if((c<a&&a<b)||(b<a&&a<c)){
-        n=a;
-    }
-    else if((b<c&&c<a)||(a<c&&c<b))
-    {
-        n=c;
-    }
-    cout<<n;
-    }
-
+  int n;
+  cout<<F(n);
 }
