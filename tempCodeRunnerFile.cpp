dab90c36cbebaccb3fae697abbd4a1adb305a92b
Gunjan <>
2024-07-11 14:45:10 +0530

    hello

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-int F(int n){
-  if(n==1||n==2){
-    return 1;
-  }
-  else
-  return F(n-1)+F(n-2);
-}
-int main()
-{
-  int n;
-  cout<<F(n);
-}
+// -->
