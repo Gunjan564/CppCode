497eb961c605c2bda7a6a470772172d3572a74d1
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2024-07-11 08:58:25 +0100

    Update STL.cpp

--- cpp/Templates/STL.cpp
+++ cpp/Templates/STL.cpp
@@ -2,4 +2,14 @@
 ---------------------STL-------------
 STL- Standard Template Library
 --> Library of generic functions and classes
+Components of STL
+1. Containers
+    • stores data
+    • Users Template classes
+2. Algorithms 
+    • Sorting 
+    • Searching 
+    • Use template functions
+3. Iterators 
+     
 */
