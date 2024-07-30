2a0de9687fea286b81a2d1627ce053220ad6835f
Gunjan <>
2024-07-30 10:57:23 +0530

    hello

--- cpp/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-complex object;
-    // complex *ptr=&object;
-    // (*ptr).setdata(1,2);
-    // (*ptr).getdata();
-    // ptr->setdata(3,4);
-    // ptr->getdata();
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,27 +1,29 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 #include <algorithm>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(string s) {
+    bool isPalindrome(string s)
+    {
         string new_s;
         string reversed_s;
 
-        for (int i = 0, j = 0; i < s.size(); i++) {
-            if ((s[i] >= 65 && s[i] <= 90)) {
-
-                new_s.push_back(s[i]+32);
-                
+        for (int i = 0, j = 0; i < s.size(); i++)
+        {
+            if ((s[i] >= 65 && s[i] <= 90))
+            {
+                new_s.push_back(s[i] + 32);
             }
-                else if (s[i] >= 97 && s[i] <= 122) {
-                    new_s.push_back(s[i]);
-                    
-                }
-            
+            else if (s[i] >= 97 && s[i] <= 122)
+            {
+                new_s.push_back(s[i]);
+            }
         }
         reverse(new_s.begin(), new_s.end());
-        cout<<new_s<<endl<<reversed_s;
+        cout << new_s << endl
+             << reversed_s;
         if (new_s == reversed_s)
             return true;
         else
@@ -31,6 +33,6 @@ public:
 int main()
 {
     Solution palindrome;
-    palindrome.isPalindrome("A man, a plan, a canal: Panama");
-return 0;
+    palindrome.isPalindrome("class 23 , class 27: hi");
+    return 0;
 }
